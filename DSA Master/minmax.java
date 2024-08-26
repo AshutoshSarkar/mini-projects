4f523dbfaf8933d2daea73fa9240648f32434f0f
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-08-26 11:25:29 +0530

    completed

---
+++ DSA Master/minmax.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+// import java.io.*;
+// public class minmax{
+//     public static void findmin(int arr[]){
+//         int end =arr.length-1;
+//         for(int i=0;i<end;i++){
+           
+//             Arrays.sort(arr);
+//             System.out.println(Arrays.toString(arr));
+//             System.out.println("the minimum number in the array is: "+arr[0]);
+//             System.out.print("the maximum number in the array is : "+arr[end]);
+//         }
+//     }
+//     public static void main(String[]args){
+//         int arr[]={3,1,2,10,9,8,6};
+      
+//         findmin(arr);
+//     }
+// }
---
+++ DSA Master/smallestelement.java
@@ -0,0 +1,4 @@
+// public class smallestelement {
+//     0
+    
+// }
